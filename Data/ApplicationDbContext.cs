1c6a7881d5448f3c03de361682b584e02be6d289
junjiebalatero <junjiebalatero@gmail.com>
2024-06-21 14:53:21 +0800

    ok

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace junjieEmployee.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Employee> Employees { get; set; }
+    }
+}
